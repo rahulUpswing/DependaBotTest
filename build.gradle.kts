5f0f16c9b6b04aa540f87efd0a518320585e07a4
rahul <rahul@upswing.one>
2024-09-09 00:22:55 +0530

    Added vulnerable dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.json:json:20230618")
 }
 
 kotlin {
