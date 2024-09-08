af8b3b1f0379e9c52a2ad1967b96bc05b8ce50c8
rahul <rahul@upswing.one>
2024-09-09 00:28:19 +0530

    Add: vulnerable dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	implementation("org.json:json:20230618")
+	implementation("org.json:json:20220924")
 }
 
 kotlin {
